328b835f5c8d9f9c88339251d620f7388e43d7b4
Nícolas Renê <nicolastonete@gmail.com>
2024-09-03 21:28:06 -0300

    incluindo mais conteúdos de Java 1.1.6

---
+++ junit5-exemplos-master/src/test/java/com/github/willyancaetano/junit/PessoaTeste.java
@@ -0,0 +1,24 @@
+package com.github.willyancaetano.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class PessoaTeste {
+
+    @Test
+    void deveCalcularIdadeCorretamente(){
+        Pessoa jessica = new Pessoa("Jéssica", LocalDateTime.of(2000, 1, 1, 15, 0, 0));
+        Assertions.assertEquals(24, jessica.getIdade());
+    }
+
+    @Test
+    void deveRetornarSeEhMaiorDeIdade(){
+        Pessoa jessica = new Pessoa("Jéssica", LocalDateTime.of(2000, 1, 1, 15, 0, 0));
+        Assertions.assertTrue(jessica.ehMaiorDeIdade());
+
+        Pessoa joao = new Pessoa("João", LocalDateTime.now());
+        Assertions.assertFalse(joao.ehMaiorDeIdade());
+    }
+}
